ad83978a4f14dda21941ccc6487975ea8647be72
co5ta <costamonzili@gmail.com>
2018-05-27 13:53:56 +0200

    Add some refactoring

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     /// The GridView contains images to assemble
     @IBOutlet weak var gridView: GridView!
     
+    /// The label mentionning the swipe direction to share the GridView
     @IBOutlet weak var swipeToShareLabel: UILabel!
     
     /// Buttons to choose the GridView layout
@@ -23,6 +24,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     /// Images in the GridView
     @IBOutlet var gridImages: [UIButton]!
     
+    /// Image tapped in the GridView
     var gridViewImageSelected: UIButton?
     
     override func viewDidLoad() {
@@ -47,77 +49,31 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        NotificationCenter.default.addObserver(self, selector: #selector(deviceRotated), name: Notification.Name.UIDeviceOrientationDidChange, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self, name: Notification.Name.UIDeviceOrientationDidChange, object: nil)
+    }
+    
+    /// Change the label indicating the swipe direction to share GridView
+    func deviceRotated() {
+        if UIDeviceOrientationIsLandscape(UIDevice.current.orientation) {
+            swipeToShareLabel.text = "Swipe left to share"
+        } else {
+            swipeToShareLabel.text = "Swipe up to share"
+        }
+        
+        gridView.setLayout(gridView.layout)
+    }
 
     /// Enable the layout change
     @IBAction func changeLayout(_ sender: UIButton) {
         unselectButtons()
         activateSelectedLayout(sender)
-    }
-    
-    /// Insert a photo in the place selected in the GridView
-    @IBAction func setGridImage(_ sender: UIButton) {
-        let imagePicker = UIImagePickerController()
-        imagePicker.delegate = self
-        
-        let alert = UIAlertController(title: "Choose a photo", message: "", preferredStyle: .actionSheet)
-        
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        alert.addAction(cancelAction)
-        
-        if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            let cameraAction = UIAlertAction(title: "Camera", style: .default) { (action) in
-                imagePicker.sourceType = .camera
-                self.present(imagePicker, animated: true, completion: nil)
-            }
-            alert.addAction(cameraAction)
-        }
-        
-        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
-            let photoLibraryAction = UIAlertAction(title: "Photo library", style: .default) { (action) in
-                imagePicker.sourceType = .photoLibrary
-                self.present(imagePicker, animated: true, completion: nil)
-            }
-            alert.addAction(photoLibraryAction)
-        }
-        
-        alert.popoverPresentationController?.sourceView = sender
-        present(alert, animated: true, completion: nil)
-        gridViewImageSelected = sender
-    }
-    
-    /// Swipe up the GridView
-    @objc private func swipeGridView(_ sender: UISwipeGestureRecognizer) {
-        guard checkIfGridViewIsFilled() else {
-            let alert = UIAlertController(title: "The grid is not filled", message: "You must fill the grid entirely if you want to share it", preferredStyle: .alert)
-            let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-            alert.addAction(cancelAction)
-            present(alert, animated: true, completion: nil)
-            return
-        }
-        
-        var transform: CGAffineTransform? = nil
-        
-        print(sender.direction)
-        print(UIDeviceOrientationIsLandscape(UIDevice.current.orientation))
-        
-        if sender.direction == .left  && UIDeviceOrientationIsLandscape(UIDevice.current.orientation) {
-            transform = CGAffineTransform(translationX: UIScreen.main.bounds.width * -1, y: 0)
-            print("landscape")
-        }
-        else if sender.direction == .up && UIDeviceOrientationIsPortrait(UIDevice.current.orientation) {
-            transform = CGAffineTransform(translationX: 0, y: UIScreen.main.bounds.height * -1)
-            print("portrait")
-        }
-        
-        if transform != nil {
-            UIView.animate(withDuration: 0.5, animations: {
-                self.gridView.transform = transform!
-            }) { (success) in
-                if success {
-                    self.shareGridView()
-                }
-            }
-        }
     }
     
     /// Unselect layout switcher buttons
@@ -142,7 +98,40 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             break
         }
     }
-
+    
+    /// Insert a photo in the box tapped in the GridView
+    @IBAction func setGridImage(_ sender: UIButton) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
+        let alert = UIAlertController(title: "Choose a photo", message: "", preferredStyle: .actionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            self.addSource(title: "Camera", imagePicker: imagePicker, sourceType: .camera, alert: alert)
+        }
+        
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            self.addSource(title: "Photo library", imagePicker: imagePicker, sourceType: .photoLibrary, alert: alert)
+        }
+        
+        alert.popoverPresentationController?.sourceView = sender
+        present(alert, animated: true, completion: nil)
+        gridViewImageSelected = sender
+    }
+    
+    /// Add source to the image picker where user can choose a photo
+    private func addSource(title: String, imagePicker: UIImagePickerController, sourceType: UIImagePickerControllerSourceType, alert: UIAlertController) {
+        let action = UIAlertAction(title: title, style: .default) { (action) in
+            imagePicker.sourceType = sourceType
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        
+        alert.addAction(action)
+    }
+    
     /// Put the choosen image to the selected place of the GridView
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage
@@ -159,53 +148,26 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         dismiss(animated: true, completion: nil)
     }
     
-    private func convertGridToImage() -> UIImage? {
-        UIGraphicsBeginImageContextWithOptions(gridView.bounds.size, true, 0)
-        gridView.drawHierarchy(in: gridView.bounds, afterScreenUpdates: true)
-        let image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        return image
-    }
-    
-    private func shareGridView() {
-        
-        guard let imageToShare = convertGridToImage() else {
+    /// Swipe the GridView
+    @objc private func swipeGridView(_ sender: UISwipeGestureRecognizer) {
+        guard checkIfGridViewIsFilled() else {
+            alertGridNotFilled()
             return
         }
-        
-        let activityViewController = UIActivityViewController(activityItems: [imageToShare], applicationActivities: nil)
-        
-        activityViewController.completionWithItemsHandler = { (activityType, completed, returnedItems, error) in
-            self.gridView.transform = .identity
-            self.gridView.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
-            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
-                self.gridView.transform = .identity
-            }, completion: nil)
+        guard let transform = getGridViewTranslationDirection(swipeDirection: sender.direction) else {
+            return
         }
-        
-        present(activityViewController, animated: true, completion: nil)
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        NotificationCenter.default.addObserver(self, selector: #selector(deviceRotated), name: Notification.Name.UIDeviceOrientationDidChange, object: nil)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        NotificationCenter.default.removeObserver(self, name: Notification.Name.UIDeviceOrientationDidChange, object: nil)
-    }
-    
-    func deviceRotated() {
-        if UIDeviceOrientationIsLandscape(UIDevice.current.orientation) {
-            swipeToShareLabel.text = "Swipe left to share"
-        } else {
-            swipeToShareLabel.text = "Swipe up to share"
+       
+        UIView.animate(withDuration: 0.5, animations: {
+            self.gridView.transform = transform
+        }) { (success) in
+            if success {
+                self.shareGridView()
+            }
         }
-        
-        gridView.setLayout(gridView.layout)
     }
     
+    /// Verify if all boxes of the GridView are filled
     private func checkIfGridViewIsFilled() -> Bool {
         let notFilled = false
         
@@ -222,5 +184,64 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         return true
     }
+    
+    /// Present an alert indicating the GridView must be filled to be shared
+    private func alertGridNotFilled() {
+        let alert = UIAlertController(title: "Grid is not filled", message: "You must fill the grid entirely if you want to share it", preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    /// Give to the GridView a suitable translation according to the swipe done
+    private func getGridViewTranslationDirection(swipeDirection: UISwipeGestureRecognizerDirection) -> CGAffineTransform? {
+        var transform: CGAffineTransform? = nil
+        
+        if swipeDirection == .left  && UIDeviceOrientationIsLandscape(UIDevice.current.orientation) {
+            transform = CGAffineTransform(translationX: UIScreen.main.bounds.width * -1, y: 0)
+        }
+        else if swipeDirection == .up && UIDeviceOrientationIsPortrait(UIDevice.current.orientation) {
+            transform = CGAffineTransform(translationX: 0, y: UIScreen.main.bounds.height * -1)
+        }
+        
+        return transform
+    }
+    
+    /// Present an activity view controller to share the GridView
+    private func shareGridView() {
+        guard let imageToShare = convertGridViewToImage() else {
+            return
+        }
+        
+        let activityViewController = UIActivityViewController(activityItems: [imageToShare], applicationActivities: nil)
+        activityViewController.completionWithItemsHandler = { (activityType, completed, returnedItems, error) in
+            self.repositionGridViewToDefaultPlace()
+        }
+        
+        present(activityViewController, animated: true, completion: nil)
+    }
+    
+    /// Convert GridView to image
+    private func convertGridViewToImage() -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(gridView.bounds.size, true, 0)
+        
+        gridView.drawHierarchy(in: gridView.bounds, afterScreenUpdates: true)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+        
+        return image
+    }
+    
+    /// Reposition the GridView to its initial place
+    private func repositionGridViewToDefaultPlace() {
+        self.gridView.transform = .identity
+        self.gridView.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
+            self.gridView.transform = .identity
+        }, completion: nil)
+    }
+    
 }
 
--- Instagrid/Model/UIImagePickerController.swift
+++ Instagrid/Model/UIImagePickerController.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2018 Co5ta. All rights reserved.
 //
 
-import Foundation
 import UIKit
 
 extension UIImagePickerController
